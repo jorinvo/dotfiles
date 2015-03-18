111776a53dd93401e4423f71a2d31aaa2eadeae3
Jorin Vogel <hi@jorin.me>
2015-03-18 14:01:16 +0700

    include atom config files and link them on bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,11 +4,19 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
+function linkAtom() {
+	# create links to atom config files
+	for file in $(pwd)/atom/*; do
+		ln -s $file ~/.atom;
+	done;
+}
+
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "brew.sh" \
 		-avh --no-perms . ~;
 	source ~/.bash_profile;
+	linkAtom;
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
