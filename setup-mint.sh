144b37d9e4dcaee26e540a1659958ba7bf48f90e
Jorin Vogel <hi@jorin.me>
2015-12-25 14:07:26 +0700

    update linux packages

--- setup-mint.sh
+++ setup-mint.sh
@@ -1,11 +1,14 @@
-sudo add-apt-repository ppa:webupd8team/ato	m
-
+sudo add-apt-repository ppa:webupd8team/atom
+sudo add-apt-repository ppa:kilian/f.lux
+sudo add-apt-repository ppa:webupd8team/tor-browser
+sudo add-apt-repository ppa:numix/ppa
 sudo add-apt-repository ppa:neovim-ppa/unstable
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 
-sudo add-apt-repository ppa:kilian/f.lux
+
+sudo apt-get update
 
 sudo apt-get install \
   git \
@@ -20,11 +23,20 @@ sudo apt-get install \
   fluxgui \
   tree \
   youtube-dl \
-  silversearcher-ag
+  silversearcher-ag \
+  tor-browser \
+  numix-gtk-theme \
+  network-manager-openvpn  \
+  owncloud-client \
+  rbenv ruby-build  \
+  cloc \
+  nmap \
+  net-tools
+
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 nvm install node
 
-sudo add-apt-repository ppa:numix/ppa
-sudo apt-get update
-sudo apt-get install numix-gtk-theme
+
+npm install -g \
+  caniuse-cmd
