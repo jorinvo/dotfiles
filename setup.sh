d6a404771a42a7f0ca53f6e3a2b211aa862fc4aa
Jorin Vogel <hi@jorin.me>
2016-08-07 18:49:16 +0200

    updating setup script with a lot of cleanup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-sudo add-apt-repository ppa:kilian/f.lux
-sudo add-apt-repository ppa:webupd8team/tor-browser
-sudo add-apt-repository ppa:numix/ppa
 sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo add-apt-repository ppa:nathan-renniewaldock/flux
+sudo add-apt-repository ppa:numix/ppa
 sudo add-apt-repository ppa:webupd8team/java
+sudo add-apt-repository ppa:webupd8team/tor-browser
 
 # Spotify
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
@@ -11,41 +11,44 @@ echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sourc
 # Owncloud client
 sudo sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/Ubuntu_16.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
 
-apt update
+sudo apt update
 
-apt install git
-apt install curl
-apt install postgresql-9.3
-apt install chromium-browser
-apt install spotify-client
-apt install fluxgui
-apt install tree
-apt install youtube-dl
-apt install silversearcher-ag
-apt install tor-browser
-apt install numix-gtk-theme
-apt install network-manager-openvpn
-apt install owncloud-client
-apt install cloc
-apt install nmap
-apt install net-tools
-apt install neovim
-apt install build-essential
-apt install cmake
-apt install jq
-apt install golang
-apt install mercurial
-apt install filezilla
-apt install slack-desktop
-apt install thunderbird
-apt install ffmpeg
-apt install xclip
-apt install numix-icon-theme-shine
-apt install trash-cli
+# Basics
+sudo apt install git
+sudo apt install curl
+sudo apt install build-essential
+sudo apt install cmake
+sudo apt install tree
+sudo apt install ffmpeg
+sudo apt install xsel
+sudo apt install trash-cli
+sudo apt install silversearcher-ag
+sudo apt install cloc
+sudo apt install postgresql
+sudo apt install youtube-dl
+sudo apt install nmap
+sudo apt install net-tools
+sudo apt install jq
+sudo apt install golang
+sudo apt install mercurial
+
+# GUIs
+sudo apt install filezilla
+sudo apt install thunderbird
+sudo apt install chromium-browser
+
+# From new repos
+sudo apt install neovim
+sudo apt install owncloud-client
+sudo apt install fluxgui
+sudo apt install numix-gtk-theme
+sudo apt install numix-icon-theme
+sudo apt install spotify-client
+sudo apt install tor-browser
 
 
 # Node
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.31.4/install.sh | bash
 nvm install node
 
 npm i -g caniuse-cmd
@@ -62,13 +65,13 @@ then
   ln -s /usr/share/vim/vim74/spell/ ~/.config/nvim/
 fi
 
-# vim-plug
+## vim-plug
 if [ ! -e ~/.config/nvim/autoload/plug.vim ]
 then
   curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   nvim -c "PlugInstall"
-  # YouCompleteMe
+  #### YouCompleteMe
   pip install --user neovim
   cd ~/.config/nvim/plugged/YouCompleteMe && ./install.py --tern-completer
 fi
@@ -78,11 +81,12 @@ fi
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 cd ~/.rbenv && src/configure && make -C src
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
-# ruby version 2.3.0 as default
+## ruby version 2.3.0 as default
 ./bin/rbenv install 2.3.0
 ./bin/rbenv global 2.3.0
-# Bundler
+## Bundler
 gem install bundler
+
 
 # Go tools
 go get github.com/spf13/hugo
